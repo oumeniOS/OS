7bd7f64953a2c15bb88201784ae834cc17728a37
yanxiaowei <xiaowei000000000@126.com>
2018-12-27 18:49:42 +0800

    tmult_ok

--- uadd_ok.c
+++ uadd_ok.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+//无符号数相加溢出判断
 //如果参数x和y相加不会产生溢出，则返回1
 //否则返回0
 int uadd_ok(unsigned x, unsigned y);
+
+//补码相加溢出检测
+//如果参数x和y相加不会产生溢出，则返回1
+//否则返回0
+int tadd_ok(int x, int y);
+
+//无符号数相乘溢出判断
+//如果参数x和y相乘不会产生溢出，则返回1
+//否则返回0
+int tmult_ok(int x, int y);
 
 int uadd_ok(unsigned x, unsigned y) {
     unsigned sum = x + y;
@@ -11,9 +22,17 @@ int uadd_ok(unsigned x, unsigned y) {
     return 1;
 }
 
+int tadd_ok(int x, int y) {
+    int sum = x + y;
+    if((x > 0 && y > 0 && sum < 0)||(x < 0 && y < 0 && sum > 0))
+        return 0;
+    return 1;
+}
 
-
-
+int tmult_ok(int x, int y) {
+    int mul = x * y;
+    return !x || p/x == y;
+}
 
 
 
@@ -24,7 +43,15 @@ void test_uadd_ok(){
     printf("uadd_ok = %d\n",uadd_ok(a, b));
 }
 
+void test_tadd_ok(){
+    //假设int 4个字节
+    int aa = 2;
+    int bb = 2147483646;
+    printf("tadd_ok = %d\n",tadd_ok(aa,bb));
+}
+
 int main () {
-    test_uadd_ok();
+    //test_uadd_ok();
+    test_tadd_ok();
     return 0;
 }
